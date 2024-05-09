e20bffe50e3123d8b6f5f88bacee93d82b2ffd41
Rugwiza <drugwiza@gmail.com>
2024-05-09 19:35:40 +0200

    Fixed the UI and made all the event listener responsive

--- script.js
+++ script.js
@@ -27,37 +27,3 @@ opButtons.forEach(button => {
   });
 });
 
-equalSign.addEventListener("click", function() {
-  let answer;
-  let a = parseFloat(previousNber);
-  let b = parseFloat(currentNber);
-  if (operation == "+") {
-      answer = a + b;
-  } else if (operation == "-") {
-      answer = a - b;
-  } else if (operation == "×") {
-      answer = a * b;
-  } else if (operation == "÷") {
-      if (b === 0){
-      display.textContent = "Error";
-      return;
-      }
-      answer = a / b;
-  } else {
-      display.textContent = "Error";
-      return;
-  }
-  result.textContent = `${previousNber} ${operation} ${currentNber}`;
-  display.textContent = answer;  
-  console.log(`Answer: ${answer}`);
-
-currentNber = answer.toString();
-       
-operation = "";
-if(operation !==""){
-result.textContent = previousNber;
-}
-console.log(`Previous Number: ${previousNber}`);
-console.log(`Current Number: ${currentNber}`);
-console.log(`Operation: ${operation}`);
-});
