99cd526670f14ab644f11d0e5ffef5480aea3f4d
Rugwiza <drugwiza@gmail.com>
2024-05-09 19:31:03 +0200

    fixed result sizes and made UI look much better

--- script.js
+++ script.js
@@ -2,31 +2,62 @@ const display = document.getElementById('display');
 const result = document.getElementById('result');
 const numberButtons = document.querySelectorAll(".numberButtons");
 const opButtons = document.querySelectorAll(".opButtons");
-const menuButtons = document.querySelectorAll("menuButtons");
+const equalSign = document.getElementById('equalSign');
 
 let previousNber = "";
 let currentNber = "";
 let operation = "";
 
 numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (operation !== "") {
-            currentNber = ""; 
-            display.textContent = "";
-            operation = "";
-        }
-        currentNber += button.textContent;
-        display.textContent += button.textContent;
-    });
+  button.addEventListener('click', () => {
+    currentNber += button.textContent;
+    display.textContent = currentNber;
+    
+  });
 });
 
 opButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (currentNber !== "") {
-            previousNber = currentNber;
-            operation = button.textContent;
-            result.textContent += `${previousNber} ${operation}`;
-            currentNber = "";
-        }
-    });
+  button.addEventListener('click', () => {
+    if (currentNber !== "") {
+      previousNber = currentNber;
+      operation = button.textContent;
+      currentNber = "";
+      result.textContent = `${previousNber} ${operation}`;
+    }
+  });
+});
+
+equalSign.addEventListener("click", function() {
+  let answer;
+  let a = parseFloat(previousNber);
+  let b = parseFloat(currentNber);
+  if (operation == "+") {
+      answer = a + b;
+  } else if (operation == "-") {
+      answer = a - b;
+  } else if (operation == "×") {
+      answer = a * b;
+  } else if (operation == "÷") {
+      if (b === 0){
+      display.textContent = "Error";
+      return;
+      }
+      answer = a / b;
+  } else {
+      display.textContent = "Error";
+      return;
+  }
+  result.textContent = `${previousNber} ${operation} ${currentNber}`;
+  display.textContent = answer;  
+  console.log(`Answer: ${answer}`);
+
+currentNber = answer.toString();
+       
+operation = "";
+if(operation !==""){
+result.textContent = previousNber;
+}
+console.log(`Previous Number: ${previousNber}`);
+console.log(`Current Number: ${currentNber}`);
+console.log(`Operation: ${operation}`);
 });
